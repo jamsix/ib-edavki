b3d856e1a4ec65d4d2ab78c6925df8449f357ae4
ddbk <ddbk@users.noreply.github.com>
2021-02-18 23:45:31 +0100

    Generate Doh-Obr with Stock Yield Enhancement Program gains

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ib_edavki",
-    version="1.3.0",
+    version="1.4.0",
     py_modules=["ib_edavki"],
     python_requires=">=3",
     entry_points={
