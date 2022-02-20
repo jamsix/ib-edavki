869cc3bd5903c3c5706145d738a64c05f382d10c
P <p@jam.si>
2022-02-20 16:34:19 +0100

    Bugfix: security matching based on various unique identificators is now following the documented order of precedence: ISIN > CUSIP > securityID > CONID > Symbol

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name="ib_edavki",
-    version="1.4.3",
+    version="1.4.4",
     py_modules=["ib_edavki", "generators.doh_obr"],
     python_requires=">=3",
     entry_points={
