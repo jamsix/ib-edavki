160b0569837269a54ef801755609b475241e2e03
jamsix <jamsix@users.noreply.github.com>
2025-02-22 15:56:54 +0100

    Update setup.py
    xml.etree.ElementTree.indent requires python 3.9 or newer

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="ib_edavki",
     version="1.4.8",
     py_modules=["ib_edavki", "generators.doh_obr"],
-    python_requires=">=3",
+    python_requires=">=3.9",
     install_requires=["requests"],
     entry_points={
         "console_scripts": ["ib_edavki=ib_edavki:main", "ib-edavki=ib_edavki:main"]
